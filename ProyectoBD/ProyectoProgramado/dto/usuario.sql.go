5af5ef73a580d6285d70af4bddafd265ddd3977f
JenyferMontano <jenifermontano32@gmail.com>
2025-10-21 12:50:27 -0600

    Eliminacion y comentacion de codigo relacionado a los procedimientos almacenados
    Se comento codiog que no se utiliza en el fron-end y estan relacionados a procedimientos almacnados. Se eliminaron archivos guias. Lo que flataria seria Base de datos, terminar de documentar triggers, lo del suuario de la Base de datos, limpiar codigo, ver que todo sirva en el front y ya estaria.

--- ProyectoBD/ProyectoProgramado/dto/usuario.sql.go
+++ ProyectoBD/ProyectoProgramado/dto/usuario.sql.go
@@ -52,13 +52,14 @@ func GetAllUsuarios(db *sql.DB) ([]GetAllUsuariosRow, error) {
 	return usuarios, nil
 }
 
-func GetCorreoByUserName(db *sql.DB, username string) (string, error) {
-	row := db.QueryRow("EXEC pa_usuario_getCorreoByUserName @userName = @p1", username)
-	var correo string
-	err := row.Scan(&correo)
-	return correo, err
-}
-
+/*
+	func GetCorreoByUserName(db *sql.DB, username string) (string, error) {
+		row := db.QueryRow("EXEC pa_usuario_getCorreoByUserName @userName = @p1", username)
+		var correo string
+		err := row.Scan(&correo)
+		return correo, err
+	}
+*/
 type GetUsuarioByCorreoRow struct {
 	Username  string         `json:"username"`
 	Password  string         `json:"password"`
@@ -109,10 +110,11 @@ func UpdateUsuario(db *sql.DB, u UpdateUsuarioParams) error {
 	return err
 }
 
+/*
 func UsuarioExiste(db *sql.DB, username string) (int64, error) {
 	row := db.QueryRow("EXEC pa_usuario_count @userName = @p1", username)
 	var count int64
 	err := row.Scan(&count)
 	return count, err
 }
-
+*/
